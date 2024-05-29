e95704e67ccb38058e8ff443ba484e25da48058f
djors <drjors2@gmail.com>
2024-05-28 21:18:26 -0400

    minor cleanup

--- src/main/java/ca/jors/learn/lang01/collaterals/UserSla.java
+++ src/main/java/ca/jors/learn/lang01/collaterals/UserSla.java
@@ -2,7 +2,6 @@ package ca.jors.learn.lang01.collaterals;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
-
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -13,9 +12,9 @@ public class UserSla implements ISla {
 
   @Setter
   private Long slaHours;
+
   private LocalDateTime startDateTime;
   private LocalDateTime endDateTime;
-
 
   public UserSla start() {
     this.startDateTime = LocalDateTime.now();
@@ -43,13 +42,13 @@ public class UserSla implements ISla {
 
   public Boolean isSlaMet() {
     return getOptionalDueDateTime()
-      .flatMap(dueDt -> {
-        return Optional.ofNullable(
+      .flatMap(dueDt ->
+        Optional.ofNullable(
           getOptionalEndDateTime()
             .map(endDt -> endDt.isBefore(dueDt))
             .orElseGet(() -> LocalDateTime.now().isAfter(dueDt) ? false : null)
-        );
-      })
+        )
+      )
       .orElse(null);
   }
 }
--- src/main/java/ca/jors/learn/lang01/service/AppService.java
+++ src/main/java/ca/jors/learn/lang01/service/AppService.java
@@ -6,6 +6,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AppService {
 
+  private AppService() {}
+
+  private static final String END_MESSAGE = "end: {}";
+  private static final String SLA_MET_MESSAGE = "slaMet?: {}";
+  private static final String DUE_MESSAGE = "due: {}";
+  private static final String DDT_MESSAGE = "ddt {}";
+
   public static void run() {
     run1();
     slaMet();
@@ -14,37 +21,37 @@ public class AppService {
 
   public static void run1() {
     var userSla = new UserSla();
-    log.info("ddt {}", userSla.getDueDateTime());
+    log.info(DDT_MESSAGE, userSla.getDueDateTime());
     userSla.setSlaHours(5L);
-    log.info("due: {}", userSla.getDueDateTime());
+    log.info(DUE_MESSAGE, userSla.getDueDateTime());
     userSla.start();
-    log.info("due: {}", userSla.getDueDateTime());
-    log.info("slaMet?: {}", userSla.isSlaMet());
+    log.info(DUE_MESSAGE, userSla.getDueDateTime());
+    log.info(SLA_MET_MESSAGE, userSla.isSlaMet());
   }
 
   public static void slaMet() {
     var userSla = new UserSla();
-    log.info("ddt {}", userSla.getDueDateTime());
+    log.info(DDT_MESSAGE, userSla.getDueDateTime());
     userSla.start();
-    log.info("due: {}", userSla.getDueDateTime());
+    log.info(DUE_MESSAGE, userSla.getDueDateTime());
     userSla.setSlaHours(5L);
-    log.info("due: {}", userSla.getDueDateTime());
-    log.info("end: {}", userSla.getEndDateTime());
+    log.info(DUE_MESSAGE, userSla.getDueDateTime());
+    log.info(END_MESSAGE, userSla.getEndDateTime());
     userSla.end();
-    log.info("end: {}", userSla.getEndDateTime());
-    log.info("slaMet?: {}", userSla.isSlaMet());
+    log.info(END_MESSAGE, userSla.getEndDateTime());
+    log.info(SLA_MET_MESSAGE, userSla.isSlaMet());
   }
 
   public static void slaNotMet() {
     var userSla = new UserSla();
-    log.info("ddt {}", userSla.getDueDateTime());
+    log.info(DDT_MESSAGE, userSla.getDueDateTime());
     userSla.start();
-    log.info("due: {}", userSla.getDueDateTime());
+    log.info(DUE_MESSAGE, userSla.getDueDateTime());
     userSla.setSlaHours(-1L);
-    log.info("due: {}", userSla.getDueDateTime());
-    log.info("end: {}", userSla.getEndDateTime());
+    log.info(DUE_MESSAGE, userSla.getDueDateTime());
+    log.info(END_MESSAGE, userSla.getEndDateTime());
     userSla.end();
-    log.info("end: {}", userSla.getEndDateTime());
-    log.info("slaMet?: {}", userSla.isSlaMet());
+    log.info(END_MESSAGE, userSla.getEndDateTime());
+    log.info(SLA_MET_MESSAGE, userSla.isSlaMet());
   }
 }
